1873659e9559add236637e8f3d6fc4a9742e6b15
ajay-teja <63445235+ajay-teja@users.noreply.github.com>
2022-04-11 11:26:20 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,4 +1,10 @@
-<?php require_once("config.php"); 
-session_destroy(); 
-header("location:login.php"); 
+<?php
+session_start();
+$_SESSION['alogin']=="";
+session_unset();
+//session_destroy();
+$_SESSION['errmsg']="You have successfully logout";
 ?>
+<script language="javascript">
+document.location="index.php";
+</script>
